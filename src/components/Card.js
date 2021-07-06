a61d6fb107e07e0785cc27571ddebe99072fb372
brurya <8462271@gmail.com>
2021-07-06 18:20:17 +0300

    change style

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Card = ({name, email, id}) => {
     return(
-        <div className = 'tc bg-light-green br3 pa3 ma2 grow bw2 shadow-5 fl w-20 pa2'>
+        <div className = 'tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
             <img alt = 'robots' src = {`https://robohash.org/${id}?200x200`} />
         <div>
             <h2>{name}</h2>
@@ -13,3 +13,5 @@ const Card = ({name, email, id}) => {
 } 
 
 export default Card;
+
+// fl w-20 pa2
